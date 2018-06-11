22e06b89623e762fee625f09f24caea98319e753
Emanuel Suriano <emanuel.suriano@gmail.com>
2018-06-11 04:20:41 +0200

    Example of light machine working!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClockWatch from './ClockWatch';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <ClockWatch />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
